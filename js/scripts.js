ec9e7c0d3a445e152719b13298c17036a18c450e
Chris Quattrocchi <chrisquattrocchi@gmail.com>
2017-11-03 13:21:29 -0700

    inputs register the correct output for pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,49 @@
+function Pizza(size, meat, veggie) {
+  this.sizePizza = size;
+  this.meatPizza = meat;
+  this.veggiePizza = veggie;
+}//end Pizza constructors
+
+Pizza.prototype.pizzaCost = function() {
+  var pizzaPrice = 0;
+  if (this.sizePizza == "sm-pizza") {
+    pizzaPrice += 8;
+  } else if (this.sizePizza == "md-pizza") {
+    pizzaPrice += 10;
+  } else if (this.sizePizza == "lg-pizza") {
+    pizzaPrice += 12;
+  } else if (this.sizePizza == "xl-pizza") {
+    pizzaPrice += 14;
+  }
+
+  if (this.meatPizza == "no-meat") {
+    pizzaPrice + 0;
+  } else if (this.meatPizza == "pepperoni-meat") {
+    pizzaPrice + 1;
+  } else if (this.meatPizza == "sausage-meat") {
+    pizzaPrice + 2;
+  }
+
+  if (this.veggiePizza == "no-veg") {
+    pizzaPrice + 0;
+  } else if (this.veggiePizza == "olive-veg") {
+    pizzaPrice + 1;
+  } else if (this.veggiePizza == "pepper-veg") {
+    pizzaPrice + 2;
+  } else if (this.veggiePizza == "onion-veg") {
+    pizzaPrice + 3;
+  }
+  return pizzaPrice;
+}//end pizzaCost prototype
+
+
 $(document).ready(function(){
   $("#order-form").submit(function(event){
     event.preventDefault();
     var pizzaSize = $("select#size-question").val();
-    alert(pizzaSize);
     var pizzaMeat = $("select#meat-question").val();
-    alert(pizzaMeat);
     var pizzaVeggie = $("select#veg-question").val();
-    alert(pizzaVeggie);
+    var newPizza = new Pizza(pizzaSize, pizzaMeat, pizzaVeggie);
+    alert(newPizza.pizzaCost());
   });//submit fxn end
 });//doc ready fxn end
